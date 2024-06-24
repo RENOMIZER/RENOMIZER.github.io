a44bb45249f97f040c36e9fae3497b03bd51a866
RENOMIZER <94466218+RENOMIZER@users.noreply.github.com>
2024-06-24 14:32:58 +0300

    Add latest thoughts

--- js/mobile.js
+++ js/mobile.js
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
     $(".text").css({"font-size": "275%", "text-shadow": "0 0 10px var(--green)"})
     $(".container").css({"border-width": "6px", "box-shadow": "0 0 10px var(--green)"})
     $(".obs-section").css("flex-flow", "column")
-    $(".game").css("margin", "0%")
+    $(".game").css("margin-left", "0%")
     $(".music > img").css("width", "100%")
   }
 })
